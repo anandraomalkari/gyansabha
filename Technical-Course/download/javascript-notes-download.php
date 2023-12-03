d503e81e703e86edfbe26072d3f9aa55865f8208
Anandrao Malkari <107610914+anandraomalkari@users.noreply.github.com>
2023-12-03 10:28:38 +0530

    main

---
+++ Technical-Course/download/javascript-notes-download.php
@@ -0,0 +1,754 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> JavaScript Notes Download | Gyansabha </title>
+    <meta name="title" content="JavaScript Notes Download | Gyansabha">
+    <meta name="description" content="Learn JavaScript by writing code, solving exercises with real-world like projects and earn certification. Best JavaScript course for beginners."/>
+    <meta name="keywords" content="JavaScript, tutorial, example, file, session, date, array, functions, time, xml, ajax, mysql, mysql, regex, string, oop, gyansabha, gyansabha.in, JavaScript Learn"/>
+    <meta name="author" content="gyansabha.in"/>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="IMG\icon.png" type="image/x-icon" />
+    <base href="https://gyansabha.in/">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8919281978138727"
+     crossorigin="anonymous"></script>
+<style>
+::-webkit-scrollbar {
+    width: 5px;
+}
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px white; 
+    border-radius: 10px;
+}
+::-webkit-scrollbar-thumb {
+    background: blue; 
+    border-radius: 5px;
+}
+::-webkit-scrollbar-thumb:hover {
+    background: gray; 
+}
+</style>
+<style>
+header{
+    position: relative;
+    max-width: 1450px;
+    margin: 0 auto;
+    padding: 10px;
+    background: #4070f4;
+    box-sizing: border-box;
+    border-radius: 4px;
+    box-shadow: 0 2px 5px rgba(0,0,0,.2);
+}
+.logo img{
+    height: 60px;
+    font-size: 40px;
+    line-height: 60px;
+    padding: 0 20px;
+    text-align: center;
+    box-sizing: border-box;
+    float: left;
+    font-weight: 700;
+    text-decoration: none;
+}
+nav{
+    float: right;
+}
+.clearfix{
+    clear: both;
+}
+nav ul{
+    margin: 0;
+    padding: 0;
+    display: flex;
+    list-style-type: none;
+}
+nav ul li{
+    list-style: none;
+}
+nav ul li a{
+    display: block;
+    margin: 10px 0;
+    padding: 10px 20px;
+    font-size: 15px;
+    text-decoration: none;
+    color: #262626;
+}
+nav ul li a.active,
+nav ul li a:hover{
+    color: #fff;
+    transition: 0.5s;
+    text-decoration: none;
+}
+@media (max-width:1200px){
+    header{
+        margin: 10px;
+    }
+    header img{
+        width:250px;
+        height: 50px;
+    }
+}
+@media (max-width:768px){
+    .menu-toggle{
+        display: block;
+        width: 40px;
+        height: 40px;
+        margin: 10px;
+        float: right;
+        cursor: pointer;
+        text-align: center;
+        font-size: 30px;
+        color: #fff;
+    }
+    .menu-toggle:before{
+        content: '\f0c9';
+        font-family: fontAwesome;
+        line-height: 40px;
+    }
+    nav{
+        display: none;
+    }
+    .menu-toggle.active:before{
+        content: '\f00d';
+    }
+    nav.active{
+        display: block;
+        width: 100%;
+    }
+    nav.active ul{
+        display: block;
+    }
+    nav.active ul li a{
+        margin: 0;
+    }
+}
+</style>
+    
+<style>
+@import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+::selection{
+  background: #4070f4;
+  color: #fff;
+}
+body{
+    background: #43464b;
+    background-position: center;
+    background-size: cover;
+    background-attachment: fixed;
+    font-family: 'Poppins', sans-serif;
+}
+.logo{
+    width: 150px;
+    margin-top: 18px;
+}
+.header{
+    height: 10%;
+}
+.header-btn{
+    text-decoration: none !important;
+    border-radius: 30px;
+    padding: 7px 20px;
+    float: right;
+    margin-top: 15px;
+    color: #fff !important;
+    background: #107afd;
+    font-weight: 600;
+}
+.subject-details{
+    margin: 8% 8%;
+}
+.left-box{
+    color: #fff;
+    margin: 15px 0;
+    padding: 20px;
+}
+.left-box h1{
+    text-transform: uppercase;
+}
+.left-box ol li{
+    list-style: none;
+    margin: 10px;
+}
+.moreimgs img{
+    height: 70px;
+    width: 70px;
+    margin-right: 10px;
+}
+.left-box a{
+    text-decoration: none !important;
+    border-radius: 30px;
+    padding: 7px 10px;
+    margin-top: 35px;
+    color: #fff !important;
+    background: #107afd;
+    font-weight: 600;
+    display: inline-block;
+    margin-right: 10px;
+}
+.download-btn{
+    border: 1px solid #fff !important;
+    background: transparent !important;
+}
+.fa{
+    margin: 0 5px;
+}
+.subject-img{
+    height: 300px;
+    box-shadow: -4px 4px 5px 0 #000;
+}
+@media (max-width:768px){
+    .subject-img{
+        margin-left: -90px;
+        width: 100%;
+    }
+}
+.series h3{
+    color: #fff;
+}
+.series img{
+    width: 100%;
+    margin-top: 15px;
+    box-shadow: -4px 4px 5px 0 #000;
+    cursor: pointer;
+    transition: 0.5s;
+}
+.series img:hover{
+    transform: translateY(-20px);
+}
+.caontainer{
+    letter-spacing: 1px;
+    position: relative;
+    width: 1300px;
+    padding: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.caontainer .card{
+    position: relative;
+    width: 350px;
+    height: 280px;
+    background: #fff;
+    margin: 10px;
+    align-content: center;
+    border-radius: 5px;
+    overflow: hidden;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.2);
+    transition: 0.5s; 
+    z-index: 1000;
+    border-top: 12px solid #4070f4;
+}
+.titles{
+    top: 0px;
+    left: 35%;
+    transform: translateX(35%);
+    padding: 15px 1px;
+    width: 200px;
+    font-size: 20px;
+    background: #4070f4;
+    color: #fff;
+    text-align: center;
+    font-size: 16px;
+    box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
+}
+.caontainer .card ol li{
+    font-size: 14px;
+    padding: 3px;
+}
+@media only screen and (max-width: 768px) {
+    .caontainer{
+        width: 100%;
+    }
+}
+.titlebar{
+    margin: 10px;
+    text-align: center;
+}
+.titlebar h1{
+    color: #fff;
+    font-family: sans-serif;
+    font-size: 24px;
+    font-weight: 900;
+}
+.containers{
+    font-family: sans-serif;
+    letter-spacing: 1px;
+    position: relative;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+ }
+ .containers .cards{
+    position: relative;
+    width: 120px;
+    height: 240px;
+    margin: 20px;
+    align-content: center;
+    border-radius: 2px;
+    overflow: hidden;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, 0.2);
+    transition: 0.5s;
+    z-index: 1000;
+ }
+</style>
+</head>
+<body>
+<header>
+    <a href="index.html" class="logo"><img src="IMG/Gyansabhalogo.png" alt="Gyansabha logo"></a>
+    <div class="menu-toggle"></div>
+    <nav>
+        <ul>
+            <li><a href="index.html" title="Home" class="active"><i class="fa fa-home" aria-hidden="true"></i> Home </a></li>
+            <li><a href="about-Gyansabha\FAQ.html" title="FAQ"><i class="fa fa-question-circle" aria-hidden="true"></i> FAQ </a></li>
+            <li><a href="about-Gyansabha\about.html" title="About"><i class="fa fa-id-card-o" aria-hidden="true"></i> About Us </a></li>
+            <li><a href="about-Gyansabha\contactus.php" title="Contact Us"><i class="fa fa-commenting-o" aria-hidden="true"></i> Contact Us </a></li>
+        </ul>
+    </nav>
+    <div class="clearfix"></div>
+</header>
+<script src="https://code.jquery.com/jquery-3.5.1.js"
+integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+crossorigin="anonymous"></script>
+<script>
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $('.menu-toggle').toggleClass('active')
+        $('nav').toggleClass('active')
+    })
+})
+</script>
+
+<style>
+.container{
+	letter-spacing: 1px;
+	position: relative;
+	width: 1350px;
+	padding: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	background: #fff;
+	box-shadow: 2px 2px 2px rgba(0,0,0,0.2);
+	border: 2px solid #4070f4;
+}
+.link{
+	display: inline-block;
+	padding: 10px;
+	background-color: #4070f4;
+	color: white;
+	font-weight: 700 0.75rem/0;
+	transition: all 0.2s;
+	text-decoration: none;
+	border-radius: 5px;
+}
+.text-container, .img-fluid{
+	margin: 10px;
+	padding: 10px;
+}
+.img-fluid img{
+	width: 100%;
+	height: 100%;
+	border-radius: 10px;
+}
+.text-container h2{
+	font-weight: 900;
+	text-transform: uppercase;
+	color: #4070f4;
+	padding: 10px;
+}
+.text-container p{
+	margin: 10px;
+	font-weight: 200;
+}
+.text-container a{
+	text-decoration: none;
+	color: #fff;
+}
+.basic-2, .basic-4{
+	padding-top: 3rem;
+	padding-bottom: 1.5rem;
+}
+.lightbox-basic{
+	position: relative;
+	max-width: 77.875rem;
+	margin: 2.5rem auto;
+	padding: 3rem 1rem;
+	border-radius: 0.25rem;
+	background-color: #fff;
+}
+@media (max-width:768px){
+	.container{
+		width: 100%;
+	}
+}
+</style>
+	<div id="details" class="basic-2">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<img class="img-fluid" src="IMG/subjects/javascript-image.png" alt="JavaScript" title="JavaScript">
+				</div>
+				<div class="col-lg-6">
+					<div class="text-container">
+					<form action="Technical-Course/download/javascript-notes-download.php" method="POST">
+					<?php
+                        $username="";//first we leave name field blank
+						$userEmail = "";//first we leave email field blank
+						$subject = "JavaScript";
+						if(isset($_POST['submit'])){ //if subscribe btn clicked
+                        $username = $_POST['username'];//getting user entered name
+						$userEmail = $_POST['email']; //getting user entered email
+                        $db = "u758960365_notes";
+                        $conn=new mysqli("localhost","u758960365_gyansabhanotes","Gyan$1bh@.com","u758960365_notes");
+                        if($conn->connect_error){
+                          die("Connection Failed:" . $conn->connect_error);
+                      }
+                      $sql = "Insert into notesdownload values('$username','$userEmail','$subject')";
+                      if($conn->query($sql)==TRUE){
+                      }
+						if(filter_var($userEmail, FILTER_VALIDATE_EMAIL)){ //validating user email
+							$subject = "JavaScript Subject Notes";
+							$message = "Dear $username,\n\n\n We are pleased to inform you that your requested JavaScript notes are ready for download. These comprehensive notes cover various aspects of JavaScript, including its syntax, data types, control structures, functions. We have compiled these notes with care, ensuring that they are well-structured and easy to understand, making them a valuable resource for both beginners and experienced developers. To access and download your JavaScript notes, please click on the following link: https://bit.ly/4300ef0. We hope you find these notes beneficial and wish you continued success in your programming journey.\n\n\n Best regards,\nGyansabha Team";
+							$sender = "From: Gyansabha <study@gyansabha.in>";
+							//php function to send mail
+							if(mail($userEmail, $subject, $message, $sender)){
+							?>
+							<!-- show sucess message once email send successfully -->
+							<div class="alert success-alert">
+								<?php echo "Your subject notes have been sent to your e-mail" ?>
+							</div>
+							<?php
+                            $username = "";
+							$userEmail = "";
+							}else{
+							?>
+							<!-- show error message if somehow mail can't be sent -->
+							<div class="alert error-alert">
+							<?php echo "Failed while sending your mail!" ?>
+							</div>
+							<?php
+							}
+						}else{
+							?>
+							<!-- show error message if user entered email is not valid -->
+							<div class="alert error-alert">
+							<?php echo "$userEmail is not a valid email address!" ?>
+							</div>
+							<?php
+						}
+						}
+						?>
+						<div class="field">
+							<input type="text" class="username" name="username" placeholder="Your Name" required value="<?php echo $username ?>">
+						</div>
+						<div class="field">
+							<input type="text" class="email" name="email" placeholder="Email Address" required value="<?php echo $userEmail ?>">
+						</div>
+						<div class="field btn">
+							<div class="layer"></div>
+							<button type="submit" name="submit">Send Me</button>
+						</div>
+						</form>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>	
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+form{
+  width: 98%;
+  padding: 30px;
+  box-shadow: 12px 12px 12px 12px rgba(0,0,0, 0.2);
+}
+form .field{
+  height: 45px;
+  width: 100%;
+  margin-bottom: 12px;
+}
+form .field input{
+  width: 100%;
+  height: 100%;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 0 15px;
+  outline: none;
+  font-size: 17px;
+  transition: all 0.3s ease;
+}
+form .field input:focus{
+  border-color: rgb(142,197,252);
+}
+form .btn{
+  height: 47px;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  border-radius: 5px;
+}
+form .btn .layer{
+  height: 100%;
+  width: 300%;
+  position: absolute;
+  left: -100%;
+  background: #4070f4;
+}
+form .btn button{
+  z-index: 1;
+  position: relative;
+  background: none;
+  padding: 0px!important;
+  color: #fff;
+  border: 0px;
+  outline: none;
+  font-size: 20px;
+  font-weight: 500;
+  cursor: pointer;
+  height: 100%;
+  width: 100%;
+}
+.wrapper .text{
+  margin-top: 5px;
+}
+.alert{
+  margin: -9px 0 12px 0;
+  padding: 10px;
+  border-radius: 5px;
+}
+.success-alert{
+  color: #155724;
+  background: #d4edda;
+  border: 1px solid #c3e6cb;
+}
+.error-alert{
+  color: #721c24;
+  background: #f8d7da;
+  border: 1px solid #f5c6cb;
+}
+</style>
+<br><br>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+footer{
+    position: absolute;
+    width: 100%;
+    background: #111;
+    color: #fff;
+}
+.main-content{
+    display: flex;
+}
+.main-content .box{
+    flex-basis: 50%;
+    padding: 10px 20px;
+}
+.box h2{
+    font-size: 1.125rem;
+    font-weight: 600;
+    text-transform: uppercase;
+}
+.box .content{
+    margin: 20px 0 0 0;
+    position: relative;
+}
+.box .content:before{
+    position: absolute;
+    content: '';
+    top: -10px;
+    height: 2px;
+    width: 100%;
+    background: #1a1a1a;
+}
+.box .content:after{
+    position: absolute;
+    content: '';
+    height: 2px;
+    width: 15%;
+    background: #4070f4;
+    top: -10px;
+}
+.left .content p{
+    text-align: justify;
+}
+.center .content .fas{
+    font-size: 2.4375rem;
+    background: #1a1a1a;
+    height: 45px;
+    width: 35px;
+    line-height: 45px;
+    text-align: center;
+    border-radius: 50%;
+    transition: 0.3s;
+    cursor: pointer;
+}
+.center .content .fas:hover{
+    background: #4070f4;
+}
+.center .content .text{
+    font-size: 1.0625rem;
+    padding-left: 10px;
+}
+.center .content .email{
+    margin: 15px 0;
+}
+.center .content .social{
+    margin: 20px 0 0 0;
+} 
+.center .content .social a{
+    padding: 0 2px;
+}
+.center .content .social a i{
+    height: 40px;
+    width: 45px;
+    background: #1a1a1a;
+    color: #fff;
+    line-height: 40px;
+    text-align: center;
+    font-size: 24px;
+    border-radius: 5px;
+    transition: 0.3s;
+}
+.center .content .social a i:hover{
+    background: #4070f4;
+}
+.right{
+    height: 45px;
+    width: 25px;
+    line-height: 30px;
+}
+.right .content .projects span a{
+    text-decoration: none;
+    color: #fff;
+}
+.right .content .projects span a:hover{
+    color: #4070f4;
+}
+.right .content .code span a{
+    text-decoration: none;
+    color: #fff;
+}
+.right .content .code span a:hover{
+    color: #4070f4;
+}
+.right .content .blog span a{
+    text-decoration: none;
+    color: #fff;
+}
+.right .content .blog span a:hover{
+    color: #4070f4;
+}
+.righttwo{
+    height: 45px;
+    width: 25px;
+    line-height: 30px;
+}
+.righttwo .content .about span a{
+    text-decoration: none;
+    color: #fff;
+}
+.righttwo .content .about span a:hover{
+    color: #4070f4;
+}
+.righttwo .content .privcy span a{
+    text-decoration: none;
+    color: #fff;
+}
+.righttwo .content .privcy span a:hover{
+    color: #4070f4;
+}
+@media screen and (max-width: 900px) {
+    footer{
+      position: relative;
+      bottom: 0px;
+    }
+    .main-content{
+      flex-wrap: wrap;
+      flex-direction: column;
+    }
+    .main-content .box{
+      margin: 5px 0;
+    }
+    .right{
+      width: 100%;
+    }
+    .righttwo{
+      width: 100%;
+    }
+}
+.footer-bottom{
+    padding: 10px;
+    background: #3d545f;
+    color: #fff;
+}
+</style>
+<footer>
+    <div class="main-content">
+        <div class="left box">
+            <h2>About us</h2>
+            <div class="content">
+                <p>Gyansabha is a place where students, Teachers and faculty members can get all the study related Things like Notes, Question Papers and Projects materials, etc. We believe that a good learner is a better explorer.</p>
+            </div>
+        </div>
+        <div class="right box">
+            <h2>Useful Links</h2>
+            <div class="content">
+                <div class="blog">
+                    <span><a href="Blog/Blog-index.html">Blog</a></span>
+                </div>
+                <div class="projects">
+                    <span><a href="Projects/Gyansabha-Projects.html">Projects</a></span>
+                </div>
+                <div class="code">
+                    <span><a href="Code/Code-index.html">Creative Code</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="righttwo box">
+            <h2>Website</h2>
+            <div class="content">
+                <div class="about">
+                    <span><a href="about-Gyansabha\about.html">About Us</a></span>
+                </div>
+                <div class="about">
+                    <span><a href="mailto:study@gyansabha.in"> Contact Us </a></span>
+                </div>
+                <div class="about">
+                    <span><a href="sitemap.xml">Sitemap</a></span>
+                </div>
+                <div class="privcy">
+                    <span><a href="about-Gyansabha\privacy-policy.html">Privacy Policy</a></span>
+                </div>
+            </div>
+        </div>
+        <div class="center box">
+            <h2>Address</h2>
+            <div class="content">
+                <div class="place">
+                    <span class="text">Solapur, Maharashtra, India</span>
+                </div>
+                <div class="email">
+                    <span class="text">study@gyansabha.in</span>
+                </div>
+                <div class="social">
+                    <a href="https://facebook.com/thegyansabha"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>
+                    <a href="https://instagram.com/thegyansabha"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                    <a href="https://www.youtube.com/channel/UCWs80Sp0N4MKV9jDnWsJVfw"><i class="fa fa-youtube-play" aria-hidden="true"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        copyright &copy; GYANSABHA 2022. All Rights Reserved.
+    </div>
+</footer>
+</body>
+</html>
